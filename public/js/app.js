7eb0a338c053418e72887b4dc186eec5d7a3f3a1
David Gómez <103974083+dabydat@users.noreply.github.com>
2022-06-06 15:16:49 -0400

    version 2 borrado

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
 console.log("Cambios de nueva rama");
 
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-
 console.log("Cambios ACTUALZIADOS de nueva rama V2");
