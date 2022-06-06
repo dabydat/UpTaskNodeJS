07c57627d67bde6bbcc865ecaf15d714942c9580
David Gómez <103974083+dabydat@users.noreply.github.com>
2022-06-06 15:51:28 -0400

    FINAL

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-console.log("Cambios de nueva rama");
-
-console.log("Cambios ACTUALZIADOS de nueva rama V2");
-console.log("Cambios ACTUALZIADOS de nueva rama V7");
-console.log("nueva rama V10");
-console.log("Cambios ACTUALZIADOS de nueva rama V4");
-console.log("Cambios ACTUAL");
-
